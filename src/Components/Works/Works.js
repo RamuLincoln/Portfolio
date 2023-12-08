0d178f7b96656da9747fb96ac093df5fe47fd987
RamuLincoln <Ramu.Valliappan@lincolnuni.ac.nz>
2023-12-08 13:06:22 +1300

    Portfolio

--- src/Components/Works/Works.js
+++ src/Components/Works/Works.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Works.css';
 import qrcode from './QRcode.png';
 import phone from './phone.png';
+import pf from './pf1.png';
 
 const Works = () => {
   return (
@@ -31,6 +32,19 @@ const Works = () => {
                     <div className='workllinks'>
                         <button className='btn'><a href='https://ramu-phone-site.netlify.app/' rel="noreferrer" target="_blank">Website</a></button>
                         <button className='btn'><a href='https://github.com/RamuLincoln/react' rel="noreferrer" target="_blank">Github</a></button>
+                    </div>
+                </div>
+            </div>
+            <div className='workscontent'>
+                <img src={pf} alt='portfolio' className='worksImg' />
+                <div className='worksdesc'>
+                    <div className='worktitle'>Project : Portfolio</div>
+                    <div className='workpara'>I have created my own Portfolio to showcase my skills. </div>
+                    <div className='worktech'>Technologies</div>
+                    <div className='workpara'>React JS</div>
+                    <div className='workllinks'>
+                        <button className='btn'><a href='https://ramu-port-folio.netlify.app' rel="noreferrer" target="_blank">Website</a></button>
+                        <button className='btn'><a href='https://github.com/RamuLincoln/Portfolio' rel="noreferrer" target="_blank">Github</a></button>
                     </div>
                 </div>
             </div>
