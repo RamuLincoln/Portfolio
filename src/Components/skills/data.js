c8e6d15cd9824a49c0a0c25942ceb6844e8893f7
RamuLincoln <Ramu.Valliappan@lincolnuni.ac.nz>
2024-01-05 15:06:50 +1300

    skills

---
+++ src/Components/skills/data.js
@@ -0,0 +1,23 @@
+
+
+const data = [
+    {
+        Id : "1",
+        Title : "UI/UX Design",
+        Desc : "I am good at figma and balsamiq ui/ux design",
+        Image : require("./UIUX.jpeg")
+    },
+    {
+        Id : "2",
+        Title : "Frontend Developer",
+        Desc : "I know HTML5, CSS, Bootstrap, JavaScript, React Js, Vue Js",
+        Image : require("./web.avif")
+    },
+    {
+        Id : "3",
+        Title : "Backend Developer",
+        Desc : "I know Python, C#, .NET, MySQL, MongoDB",
+        Image : require("./mob.avif")
+    }
+]
+export default data;
